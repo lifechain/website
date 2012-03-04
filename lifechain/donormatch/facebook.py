21f5f7a4e0ca9b0306e506e4fd86df87a8b6a0b9
Nick Sullivan <nick@sullivanflock.com>
2012-03-04 16:12:27 +0000

    facebook friend list stored in user profile

--- lifechain/donormatch/facebook.py
+++ lifechain/donormatch/facebook.py
@@ -1,7 +1,23 @@
-from social_auth.signals import pre_update
-from social_auth.backends.facebook import FacebookBackend
 from donormatch.models import UserProfile
+from social_auth.signals import pre_update
+from social_auth.backends.facebook import FacebookBackend, FACEBOOK_ME
+from urllib2 import urlopen
+from urllib import urlencode
 import datetime
+import simplejson
+
+def fetch_facebook_friends(user, response):
+    access_token = response['access_token']
+    url = FACEBOOK_ME.rstrip('?') + '/friends?' + urlencode({'access_token': access_token})
+    print url
+    try:
+        data = simplejson.load(urlopen(url))
+    except ValueError:
+        print 'Could not load friend list from Facebook.'
+        data = {}
+    return data
+
+
 
 def facebook_extra_values(sender, user, response, details, **kwargs):
     try:
@@ -11,7 +27,6 @@ def facebook_extra_values(sender, user, response, details, **kwargs):
         # Must be a new user, create one
         userprofile = UserProfile(user = user)
 
-    print response.get("birthday")
     try:
         birthday = datetime.datetime.strptime(response.get("birthday"), '%m/%d/%Y')
         userprofile.birthday = birthday
@@ -27,8 +42,13 @@ def facebook_extra_values(sender, user, response, details, **kwargs):
     elif response.get("gender") == "female":
         userprofile.sex = 'f'
 
+    facebook_friends = fetch_facebook_friends(user, response)
+    userprofile.facebook_friends = simplejson.dumps(facebook_friends["data"])
+
     userprofile.save()
 
     return True
 
 pre_update.connect(facebook_extra_values, sender=FacebookBackend)
+
+
