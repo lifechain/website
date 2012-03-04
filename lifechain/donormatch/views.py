79031fa80498a7f6aca2fdb4818c6ac749231e29
Michael Leonhard <mike@restbackup.com>
2012-03-04 21:06:21 +0000

    Added home page text, fblogin button

--- lifechain/donormatch/views.py
+++ lifechain/donormatch/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-    if request.user.is_authenticated():
-        return render_to_response("home.html", {}, RequestContext(request))
-    else:
-        return render_to_response("landing.html", {}, RequestContext(request))
+    return render_to_response("home.html", {}, RequestContext(request))
