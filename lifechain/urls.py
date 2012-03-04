2f3f3acb5ecd177a095c868d50558565da05651a
Nick Sullivan <nick@sullivanflock.com>
2012-03-04 05:54:01 +0000

    login as facebook

--- lifechain/urls.py
+++ lifechain/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'donormatch.views.home', name='home'),
+
+    # Django social auth https://github.com/omab/django-social-auth
+    url(r'', include('social_auth.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^logout$', 'django.contrib.auth.views.logout'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
