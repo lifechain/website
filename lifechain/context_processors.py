58b027bd915f817fb7f32c1f64f6c322eea9dd6d
Michael Leonhard <mike@restbackup.com>
2012-03-04 23:37:32 +0000

    Initialize personal data form with data from db

--- lifechain/context_processors.py
+++ lifechain/context_processors.py
@@ -12,7 +12,17 @@ def lifechain_context(request):
             lifechain_context["facebook_access_token"] = access_token
 
     lifechain_context["loggedin"] = request.user.is_authenticated()
-    lifechain_context["user"] = str(request.user)
+    lifechain_context["username"] = str(request.user)
+    # lifechain_context["first_name"] = str(request.user.first_name)
+    # lifechain_context["last_name"] = str(request.user.last_name)
+    # lifechain_context["email"] = str(request.user.email)
+    # lifechain_context["birthday_date"] = str(request.user.userprofile.birthday)
+    # #lifechain_context["facebook_friends"] = str(request.user.userprofile.facebook_friends)
+    # lifechain_context["lattitude"] = str(request.user.userprofile.lattitude)
+    # lifechain_context["longitude"] = str(request.user.userprofile.longitude)
+    # lifechain_context["location"] = str(request.user.userprofile.location)
+    # lifechain_context["phone"] = str(request.user.userprofile.phone)
+    # lifechain_context["sex"] = str(request.user.userprofile.sex)
 
     context["lifechain_context"] = simplejson.dumps(lifechain_context, indent=1)
     return context
