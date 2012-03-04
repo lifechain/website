8797cfae47f544f3d022a0f08e91bc10c49f3b28
Michael Leonhard <mike@restbackup.com>
2012-03-04 07:00:20 +0000

    fixing merge

--- lifechain/settings.py
+++ lifechain/settings.py
@@ -119,7 +119,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'donormatch',
-    'social_auth'
+    'social_auth',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'tastypie',
