1ecadbcf621e7677e20593fddcdc8604ca5fe66e
Michael Leonhard <mike@restbackup.com>
2012-03-04 20:07:02 +0000

    checking in landing page

--- lifechain/donormatch/views.py
+++ lifechain/donormatch/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-    return render_to_response("home.html", {}, RequestContext(request))
+    if request.user.is_authenticated():
+        return render_to_response("home.html", {}, RequestContext(request))
+    else:
+        return render_to_response("landing.html", {}, RequestContext(request))
